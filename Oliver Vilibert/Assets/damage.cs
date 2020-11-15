f7641226b4e17050cf4b47846a621df4b1c6274a
Arkudaa <71982604+Arkudaa@users.noreply.github.com>
2020-11-15 03:27:40 -0800

    version 1.01

---
+++ Oliver Vilibert/Assets/damage.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class damage : MonoBehaviour
+{
+    // Start is called before the first frame update
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if(collision.gameObject.tag == "wall")
+        {
+            collision.gameObject.SendMessage("Takedamage");
+        }
+    }
+}
---
+++ Oliver Vilibert/Assets/wall.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class wall : MonoBehaviour
+{
+    public int health;
+    public GameObject explosion;
+    // Start is called before the first frame update
+
+    public void Takedamage()
+    {
+        health--;
+        if (health <= 0)
+        {
+            Destroy(gameObject);
+            GameObject f= Instantiate(explosion, transform.position, transform.rotation);
+            Destroy(f, 6);
+        }
+    }
+}
+
+
+
+
+
+
+
+
