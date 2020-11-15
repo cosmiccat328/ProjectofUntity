bb4d8acaf7b83267537c20145959cb7dec6c4634
Arkudaa <71982604+Arkudaa@users.noreply.github.com>
2020-11-15 04:32:47 -0800

    version 1.011

--- Oliver Vilibert/Assets/damage.cs
+++ Oliver Vilibert/Assets/damage.cs
@@ -11,6 +11,12 @@ public class damage : MonoBehaviour
         if(collision.gameObject.tag == "wall")
         {
             collision.gameObject.SendMessage("Takedamage");
+
+            if (collision .gameObject.tag == "npc");
+            {
+                collision.gameObject.SendMessage("Takedamage");
+
+            }
         }
     }
 }
