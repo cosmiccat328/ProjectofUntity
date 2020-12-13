8a6e0c40f1f9334356dc0aa952f2182b90f057bc
Arkudaa <71982604+Arkudaa@users.noreply.github.com>
2020-12-13 04:30:56 -0800

    ver 1.00005

---
+++ Oliver Vilibert/Assets/Buttonmanager.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Buttonmanager : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public void StartGame()
+    {
+        Application.LoadLevel(1);
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+}
